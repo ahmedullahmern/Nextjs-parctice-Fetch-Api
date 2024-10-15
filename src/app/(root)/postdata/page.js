20754d7c5f1d45f4f2aaf2d33acde6b1cdd9dbda
ahmedullahmern <ahmedullahmern@gmail.com>
2024-10-15 19:09:54 +0500

    alterration

--- src/app/(root)/postdata/page.js
+++ src/app/(root)/postdata/page.js
@@ -1,14 +1,14 @@
 export default async function post() {
-    let res = await fetch('https://dummyjson.com/posts')
-    res = await res.json()
+    let res = await fetch('https://dummyjson.com/posts');
+    res = await res.json();
 
     return (
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
-                {
-                    res.posts.map((data) => (
-                        <div className="flex flex-wrap  -m-4">
-                            <div className="p-4 lg:w-1/3">
+                <div className="flex flex-wrap -m-4">
+                    {
+                        res.posts.map((data) => (
+                            <div className="p-4 lg:w-1/3" key={data.id}>
                                 <div className="bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
                                     <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
                                         {data.title}
@@ -33,7 +33,9 @@ export default async function post() {
                                                 <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
                                                 <circle cx={12} cy={12} r={3} />
                                             </svg>
-                                            1.2K
+                                            {
+                                                data.views
+                                            }
                                         </span>
                                         <span className="text-gray-400 inline-flex items-center leading-none text-sm">
                                             <svg
@@ -47,16 +49,17 @@ export default async function post() {
                                             >
                                                 <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
                                             </svg>
-                                            6
+                                            {
+                                                data.reactions.likes
+                                            }
                                         </span>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
         </section>
-
-    )
+    );
 }
